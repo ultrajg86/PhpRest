dce8748c23c3a3f4b207a27cd43c69977c49d60e
jongs <ultrajg86@nate.com>
2017-02-03 16:19:16 +0900

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+
+$app->get('/', function (Request $request, Response $response) {
+    var_dump($request);
+    var_dump($response);
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->run();
+
+?>
