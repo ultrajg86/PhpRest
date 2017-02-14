001b46e9282a509a92a7ddab518fc1733576fcc0
jongs <ultrajg86@nate.com>
2017-02-14 16:04:50 +0900

    clean index.php

--- app/middleware.php
+++ app/middleware.php
@@ -5,3 +5,26 @@
  * Date: 2017-02-14
  * Time: 오전 10:45
  */
+
+//middleware
+
+// routes...
+$app->add(function ($request, $response, callable $next) {
+    $route = $request->getAttribute('route');
+
+    // return NotFound for non existent route
+    if (empty($route)) {
+        throw new NotFoundException($request, $response);
+    }
+
+    $name = $route->getName();
+    $groups = $route->getGroups();
+    $methods = $route->getMethods();
+    $arguments = $route->getArguments();
+
+    //var_dump($name, $groups, $methods, $arguments);
+    $args = array('name'=>$name, 'groups'=>$groups, 'methods'=>$methods, 'arguments'=>$arguments);
+
+    // do something with that information
+    return $next($request, $response, $args);
+});
--- app/routes.php
+++ app/routes.php
@@ -5,3 +5,95 @@
  * Date: 2017-02-14
  * Time: 오전 10:45
  */
+
+
+//public route
+$app->group('', function(){
+
+    $this->get('/', function ($request, $response, $args) {
+        return $this->view->render($response, 'index.php', ['type'=>'main']);
+    });
+
+    $this->get('/contact', function ($request, $response, $args) {
+        var_dump($args);
+        return $this->view->render($response, 'contact/contact.php');
+    });
+
+    $this->get('/notice', function ($request, $response, $args) {
+        return $this->view->render($response, 'notice/list_page.php');
+    });
+
+    $this->get('/notice/{idx}', function ($request, $response, $args) {
+        return $this->view->render($response, 'notice/view_page.php');
+
+    });
+
+    $this->group('/user', function(){
+
+        $this->get('', function ($request, $response, $args) {
+            return $this->view->render($response, 'profile.php', [
+                'name' => $args['name']
+            ]);
+        });
+
+        $this->get('/login', function ($request, $response, $args) {
+            return $this->view->render($response, 'profile.php', [
+                'name' => $args['name']
+            ]);
+        });
+
+        $this->post('/user', function ($request, $response) {
+            $body = $request->getBody()->getContents();
+            var_dump($body);
+            return $response;
+        });
+
+    });
+
+    $this->post('/upload', function ($request, $response) {
+        $files = $request->getUploadedFiles();
+        if (empty($files['newfile'])) {
+            throw new Exception('Expected a newfile');
+        }
+
+        $newfile = $files['newfile'];
+        // do something with $newfile
+
+        if ($newfile->getError() === UPLOAD_ERR_OK) {
+            $uploadFileName = $newfile->getClientFilename();
+            $newfile->moveTo($_SERVER['DOCUMENT_ROOT'].'/uploads/' . date('Y') . '/' . $uploadFileName);
+        }
+
+    });
+
+});
+
+//admin route
+$app->group('/admin', function(){
+
+    $this->get('/', function ($request, $response){
+        $data = array('name' => 'Rob', 'age' => 40);
+        $newResponse = $response->withJson($data, 200);
+        return $newResponse;
+    });
+
+});
+
+
+//api route
+$app->group('/api', function(){
+
+    $this->get('', function ($request, $response){
+        $data = array('name' => 'Rob', 'age' => 40);
+        $newResponse = $response->withJson($data, 200);
+        return $newResponse;
+    });
+
+});
+
+// Render Twig template in route
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'profile.php', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
